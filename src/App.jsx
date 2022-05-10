f56a8ff69cda4bb381c7cfc3050daaac9de63cb5
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-09 21:25:30 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from "@emotion/styled";
+
+const Heding = style.h1`
+ font-family: 'Lato', sans-serif;
+ color:#fff;
+`;
+
+function App() {
+  return <Heding>Desde app</Heding>;
+}
+
+export default App;
