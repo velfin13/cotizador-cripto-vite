f4f58280f12edf95ce683dfe2e5e47aca4c5e751
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-09 22:47:41 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import style from "@emotion/styled";
+import styled from "@emotion/styled";
 import Formulario from "./components/Formulario";
 import ImagenCripto from "./img/imagen-criptos.png";
 
-const Contenedor = style.div`
+const Contenedor = styled.div`
   max-width:900px;
   margin: 0 auto;
   width:90%;
@@ -14,14 +14,14 @@ const Contenedor = style.div`
   }
 `;
 
-const Imagen = style.img`
+const Imagen = styled.img`
   max-width:400px;
   width:80%;
   margin:100px auto 0 auto;
   display:block
 `;
 
-const Heding = style.h1`
+const Heding = styled.h1`
  font-family: 'Lato', sans-serif;
  color:#fff;
  text-align: center;
