7d4e02b290e1fc357527c3fb0b2ebf32a5457ecc
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-09 22:33:51 -0500

    Escribiendo el CSS de nuestro proyecto con Styled Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import React from 'react'
-import style from "@emotion/styled";
+import React from "react";
+import style from "styled-components";
+import ImagenCripto from "./img/imagen-criptos.png";
+
+const Contenedor = style.div`
+  max-width:900px;
+  margin: 0 auto;
+  width:90%;
+  @media(min-width:992px){
+    display:grid;
+    grid-template-columns:repeat(2,1fr);
+    column-gap:2rem;
+  }
+`;
+
+const Imagen = style.img`
+  max-width:400px;
+  width:80%;
+  margin:100px auto 0 auto;
+  display:block
+`;
 
 const Heding = style.h1`
  font-family: 'Lato', sans-serif;
  color:#fff;
+ text-align: center;
+ font-weight: 700;
+ margin-top: 80px;
+ margin-bottom: 50px;
+ font-size: 34px;
+
+
+ &::after {
+  content: "";
+  width: 100px;
+  height: 6px;
+  background-color: #66a2fe;
+  display: block;
+  margin: 10px auto auto;
+}
 `;
 
 function App() {
-  return <Heding>Desde app</Heding>;
+  return (
+    <Contenedor>
+      <Imagen src={ImagenCripto} alt="fondo" />
+      <div>
+        <Heding>Cotiza Criptomonedas al Instante</Heding>
+      </div>
+    </Contenedor>
+  );
 }
 
 export default App;
