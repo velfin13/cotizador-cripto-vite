2eaaf7c746573a757b469c2439510a4d0c6c4452
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2022-05-10 15:52:20 -0500

    select de monedas

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "@emotion/styled";
+import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
 
 const InputSubmit = styled.input`
   background-color: #9497ff;
@@ -11,17 +13,22 @@ const InputSubmit = styled.input`
   text-transform: uppercase;
   font-size: 20px;
   border-radius: 8px;
-  transition: background-color .3s ease;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
 
-  &:hover{
+  &:hover {
     background-color: #7a7dfe;
     cursor: pointer;
   }
 `;
 
 const Formulario = () => {
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+
   return (
     <form>
+      <SelectMonedas />
+      {state}
       <InputSubmit type="submit" value={"Cotizar"} />
     </form>
   );
